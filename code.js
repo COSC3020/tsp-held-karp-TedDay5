d4484c8cb75edeb91fc5c71352a3e239dceb3f10
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-11-30 10:56:51 -0700

    implemented tsp_hk with heldKarp function within

--- code.js
+++ code.js
@@ -1,3 +1,46 @@
 function tsp_hk(distance_matrix) {
-    return -1;
+    let l = distance_matrix.length;
+    let cache = new Map();
+
+    if (l <= 1) {
+        return 0;
+    }
+
+
+    function heldKarp(cities, start) {
+        if(cities.size === 1) {
+            let remaining = array.from(cities)[0];
+            return distance_matrix[start][remaining];
+        }
+
+        let key = '${array.from(cities).join('-')}-${start}'
+        let minimumTour = Infinity;
+
+        if(cache.has(key)) {
+            return cache.get(key);
+        }
+        for(let city of cities) {
+            if(city !== start) {
+                let citiesSet = new Set(cities);
+
+                citiesSet.delete(city)
+
+                let tour = heldKarp(citiesSet, city) + distance_matrix[start][city];
+
+                minimumTour = Math.min(minimumTour, tour);
+            }
+        }
+        cache.set(key, minimumTour);
+        return minimumTour;
+    }
+    let minimumTour = Infinity;
+
+    for (let start = 0; start < l; start++){
+        let cities = new Set([...array(l).keys()].filter((c) => c !== start));
+        let tour = heldKarp(cities, start);
+
+        minimumTour = Math.min(minimumTour, tour);
+    }
+    cache.clear();
+    return minimumTour;
 }
