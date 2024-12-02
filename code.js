589e346ac471f0f3140fc13a3cb80fb21d3a394e
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-12-02 03:25:36 -0700

    restructured and reworked

--- code.js
+++ code.js
@@ -1,46 +1,57 @@
 function tsp_hk(distance_matrix) {
     let l = distance_matrix.length;
-    let cache = new Map();
 
     if (l <= 1) {
         return 0;
     }
-
-
-    function heldKarp(cities, start) {
-        if(cities.size === 1) {
-            let remaining = Array.from(cities)[0];
-            return distance_matrix[start][remaining];
-        }
-
-        let key = '${Array.from(cities).join('-')}-${start}'
-        let minimumTour = Infinity;
-
-        if(cache.has(key)) {
-            return cache.get(key);
-        }
-        for(let city of cities) {
-            if(city !== start) {
-                let citiesSet = new Set(cities);
-
-                citiesSet.delete(city)
-
-                let tour = heldKarp(citiesSet, city) + distance_matrix[start][city];
-
-                minimumTour = Math.min(minimumTour, tour);
-            }
-        }
-        cache.set(key, minimumTour);
-        return minimumTour;
-    }
     let minimumTour = Infinity;
 
     for (let start = 0; start < l; start++){
-        let cities = new Set([...Array(l).keys()].filter((c) => c !== start));
-        let tour = heldKarp(cities, start);
+        let cache = {};
+        let cities = [];
 
-        minimumTour = Math.min(minimumTour, tour);
+        for(let i = 0; i < l; i++) {
+            cities.push(i);
+        }
+        let tour = heldKarp(distance_matrix, cities, start, cache);
+
+        if(tour < minimumTour) {
+            minimumTour = tour;
+        }
     }
-    cache.clear();
+    return minimumTour;
+}
+
+function heldKarp(distance_matrix, cities, start, cache) {
+    let key = '${Array.from(cities).join('-')}-${start}';
+
+    if(cache.has(key)) {
+        return cache.get(key);
+    }
+
+    if(cities.length == 2) {
+        let remaining = cities.find(c => c !== start);
+        return cache.get(key);
+    }
+    let minimumTour = Infinity;
+
+    for(let city = 0; city < cities.length; city++) {
+        if(city !== start) {
+            let citiesSet = [];
+
+            for(let newCity = 0; newCity < cities.length; newCity++) {
+                if(newCity !== start) {
+                    citiesSet.push(newCity);
+                }
+            }
+
+            let tour = heldKarp(distance_matrix, citiesSet, city, cache) + distance_matrix[start][city];
+
+            if(tour < minimumTour) {
+                minimumTour = tour;
+            }
+        }
+    }
+    cache.set(key, minimumTour);
     return minimumTour;
 }
